0eee7e6fce3ee9b5aadf274f56f34017ecd8bb2d
tpcaibu <tpcai@bu.edu>
2023-09-12 04:15:45 +0000

    formatting

--- fleaapi/helloworld/helloworld.py
+++ fleaapi/helloworld/helloworld.py
@@ -1,6 +1,7 @@
 from django.http import JsonResponse
 from django.views.decorators.http import require_GET
 
+
 @require_GET
 def helloworld(request):
-    return JsonResponse({'message': 'Hello, world!'})
+    return JsonResponse({"message": "Hello, world!"})
--- fleaapi/helloworld/unittest/helloworld_test.py
+++ fleaapi/helloworld/unittest/helloworld_test.py
@@ -3,11 +3,13 @@ from unittest.mock import MagicMock
 
 from ..helloworld import *
 
+
 class HelloWorldTest(TestCase):
     def setUp(self) -> None:
         self.request = MagicMock()
         self.request.method = "GET"
 
     def test_helloworld(self):
-        self.assertJSONEqual(helloworld(self.request).content, {'message': 'Hello, world!'})
-
+        self.assertJSONEqual(
+            helloworld(self.request).content, {"message": "Hello, world!"}
+        )
