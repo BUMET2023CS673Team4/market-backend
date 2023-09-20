c490af7f0514b832cd22a4614571e97b80132575
tpcaibu <tpcai@bu.edu>
2023-09-20 19:55:34 +0000

    add ci/cd to azure

--- fleaapi/helloworld/helloworld.py
+++ fleaapi/helloworld/helloworld.py
@@ -1,7 +1,10 @@
-from django.http import JsonResponse
+from django.http import HttpRequest, HttpResponse, JsonResponse
 from django.views.decorators.http import require_GET
 
 
 @require_GET
-def helloworld(request):
+def helloworld(request: HttpRequest) -> HttpResponse:
+    """
+    Sample API endpoint that returns a JSON response.
+    """
     return JsonResponse({"message": "Hello, world!"})
