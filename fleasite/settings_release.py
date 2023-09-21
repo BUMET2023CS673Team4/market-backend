020c4b56b725344a216ab0fa03b2b03ca6581961
tpcaibu <tpcai@bu.edu>
2023-09-21 03:19:12 +0000

    fix formatting

--- fleasite/settings_release.py
+++ fleasite/settings_release.py
@@ -8,4 +8,6 @@ SECRET_KEY = "django-insecure-vb_08&&o(0e9_o&#^ucukz091h6d0o7eiw01$0gsm&&v&kdmd5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME'] if 'WEBSITE_HOSTNAME' in os.environ else 'localhost']
+ALLOWED_HOSTS = [
+    os.environ['WEBSITE_HOSTNAME'] if 'WEBSITE_HOSTNAME' in os.environ else 'localhost'
+]
