ffe9281473d8fe434a3bc7994ff8613243364c9a
tpcaibu <tpcai@bu.edu>
2023-09-21 03:14:56 +0000

    update scripts and settigns for azure

---
+++ fleasite/settings_release.py
@@ -0,0 +1,11 @@
+import os
+
+from .settings_common import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-vb_08&&o(0e9_o&#^ucukz091h6d0o7eiw01$0gsm&&v&kdmd5"
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME'] if 'WEBSITE_HOSTNAME' in os.environ else 'localhost']
