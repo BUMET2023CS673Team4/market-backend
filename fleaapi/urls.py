65491b7975cf1dbd02ce3ffa571c0611e774cbfd
tpcaibu <tpcai@bu.edu>
2023-12-02 05:41:57 +0000

    Revert "Revert "updated repo folders""
    Working feature branch
    This reverts commit 120a8023845e419cfe2a3c4ab322e52d2e3fe063.

--- fleaapi/urls.py
+++ fleaapi/urls.py
@@ -10,4 +10,10 @@ urlpatterns = [
     path("stripe-public-key/", checkout.stripe_public_key),
     path("create-checkout-session/", checkout.create_checkout_session),
     path("session-status/", checkout.session_status),
+<<<<<<< HEAD
+=======
+    # path("forgotpassword/", user.forgotpassword),
+    path('request-password-reset/', request_password_reset, name='request_password_reset'),
+    path('reset-password/', user.reset_password, name='reset_password'),
+>>>>>>> parent of 120a802 (Revert "updated repo folders")
 ]
