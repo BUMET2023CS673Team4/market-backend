f247f9eca7e3371c262ab435bbe2c0c5d2640731
Your Name <you@example.com>
2023-12-03 13:48:09 +0800

    finish lint

--- fleaapi/product_detail/product_detail.py
+++ fleaapi/product_detail/product_detail.py
@@ -1,24 +1,26 @@
-from django.views.decorators.http import require_POST
-
 from django.http import (
     HttpRequest,
     HttpResponse,
     HttpResponseBadRequest,
     HttpResponseServerError,
 )
+from django.views.decorators.http import require_POST
 
 from fleaapi.models import Item
 
+
 @require_POST
-def get_product(request: HttpRequest) -> HttpResponse :
+def get_product(request: HttpRequest) -> HttpResponse:
     search_name = request.POST.get("name")
-    #search for the item
-    if not name :
+    # search for the item
+    if not name:
         return HttpResponseBadRequest('Neea parameter called name!')
     try:
         result_items = Item.objects.filter(name=search_name)
         serialized_data = serialize('json', result_items)
         return JsonResponse({'items': serialized_data}, safe=False)
-    
+
     except Item.DoesNotExist:
-        return JsonResponse({'error': f'Item with name {item_name} not found'}, status=404)
+        return JsonResponse(
+            {'error': f'Item with name {item_name} not found'}, status=404
+        )
