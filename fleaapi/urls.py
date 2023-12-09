18e2afaf3ef8b8023fa8ea0e329328588e217130
Tingtao Zhang <144845636+Timtaoz@users.noreply.github.com>
2023-12-09 15:20:18 -0500

    funtions in cart and product page (#28)
    show_items_in_cart
    get_product_by_id
    add_item_to_cart
    few tests
    
    Co-authored-by: chronoszh <chronos@crc-dot1x-nat-10-239-1-198.bu.edu>

--- fleaapi/urls.py
+++ fleaapi/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from . import views
-from .controllers import checkout, user
+from .controllers import checkout, user, product,cart
 
 urlpatterns = [
     path("helloworld/", views.helloworld),
@@ -10,4 +10,7 @@ urlpatterns = [
     path("stripe-public-key/", checkout.stripe_public_key),
     path("create-checkout-session/", checkout.create_checkout_session),
     path("session-status/", checkout.session_status),
+    path("get-product-by-id/", product.get_product_by_id),
+    path("add-item-to-cart/", product.add_item_to_cart),
+    path("show-items-in-cart/", cart.show_items_in_cart),
 ]
